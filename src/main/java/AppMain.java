7634c7d88992b8d4de0f4c3ccd2e887dba7b9048
Jan Kraus <jan.erik.kraus@t-online.de>
2022-05-18 11:01:25 +0200

    Adding Maven Testproject

---
+++ src/main/java/AppMain.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+        System.out.println("Taschenrechner \nAufgabe 1:");
+
+        int s1;
+        int s2;
+        String operator;
+        int result = 0;
+        String resultText = "Ergebnis: ";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Erste Zahl eingeben:");
+        s1 = scanner.nextInt();
+        System.out.println("Zweite Zahl eingeben:");
+        s2 = scanner.nextInt();
+
+        System.out.println("Operator eingeben");
+        operator = scanner.next();
+        operator.replaceAll(" ", "");
+
+        if (operator.equals("+")){
+            result = s1 + s2;
+        } else if (operator.equals("-")) {
+            result = s1 - s2;
+        } else if (operator.equals("*")) {
+            result = s1 * s2;
+        } else if (operator.equals("/")) {
+            result = s1 / s2;
+        } else if (operator.equals("%")) {
+            result = s1 % s2;
+        } else {
+            System.out.println("Der eingegebene Operator funktioniert nicht.");
+        }
+
+        System.out.println(s1 + operator + s2 + "=");
+        System.out.println(resultText + "\n" + result);
+
+        System.out.println("Zahlen vergleichen \nAufgabe 2:");
+
+        System.out.println("Erste Zahl eingeben:");
+        s1 = scanner.nextInt();
+        System.out.println("Zweite Zahl eingeben:");
+        s2 = scanner.nextInt();
+
+        if(s1!=s2){
+            if(s1<s2){
+                System.out.println(s1 + " ist kleiner als " + s2);
+            } else {
+                System.out.println(s1 + " ist größer als " + s2);
+            }
+        } else {
+            System.out.println(s1 + " und " + s2 + " sind gleich.");
+        }
+
+    }
+}
