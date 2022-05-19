089062e8afa53a96d7ba1fe407e46446ccde7117
Jan Kraus <jan.erik.kraus@t-online.de>
2022-05-19 14:51:04 +0200

    Student-Array-Generator: Test added

--- src/test/java/GenerateStudentArrayTest.java
+++ src/test/java/GenerateStudentArrayTest.java
@@ -7,13 +7,15 @@ class GenerateStudentArrayTest {
         //Given
         int studentsAmount = 5;
         String expected = "Student 4";
+        String[] expected2 = {"Student 1", "Student 2", "Student 3", "Student 4", "Student 5"};
 
         //When
         String[] result = GenerateStudentArray.generate(studentsAmount);
         String actual = result [3];
+        String[] actual2 = result;
 
         //Then
         Assertions.assertEquals(expected, actual);
-
+        Assertions.assertArrayEquals(expected2, actual2);
     }
 }
